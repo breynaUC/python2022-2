406cf60311f38b28eb4cd7b3b5852c884f39bf4e
Docente <docente@Cientifica.pe>
2022-11-03 08:21:28 -0500

    clase10

---
+++ ejer01.py
@@ -0,0 +1,14 @@
+
+cadena1 = "Hola mundo"
+cadena2 = "tu puedes conquistarlo"
+print(cadena1+ " " +cadena2)
+print(len(cadena1))
+#ejemplo2
+cad = input("Ingresar nombre: ")
+if len(cad)>=8:
+    print(cad)
+else:
+    print("Inresar cadena mayor a 8 caracteres")
+    
+
+    
